e59f5d1a5654907e38169b982f199aa14db85458
aszxy-udin <agusbangle53@gmail.com>
2025-07-11 15:54:29 +0700

    music.js

---
+++ music.js
@@ -0,0 +1,20 @@
+const playlist = [
+  {
+    title: "Surat Cinta Untuk Starla",
+    artist: "Virgound",
+    src: "https://files.catbox.moe/a2aa4u.mp3",
+    cover: "https://files.catbox.moe/v6jhwx.jpg"
+  },
+  {
+    title: "Gorengan Habis",
+    artist: "Akustik Angkringan",
+    src: "https://example.com/audio/gorengan.mp3",
+    cover: "https://example.com/images/gorengan.jpg"
+  },
+  {
+    title: "Obrolan Malam Jumat",
+    artist: "PAF Sound",
+    src: "https://example.com/audio/malam-jumat.mp3",
+    cover: "https://example.com/images/malam.jpg"
+  }
+];
